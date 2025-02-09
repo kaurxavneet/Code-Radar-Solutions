aae85bbbb4662e6566d2c50e9891dc4866934801
kaurxavneet <avneet0875.becse24@chitkara.edu.in>
2025-02-09 17:00:34 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%d".&a);
+    if(a=='A')
+    {
+        printf("Excellent");
+    }
+     if(a=='B')
+    {
+        printf("Good");
+    }
+     if(a=='C')
+    {
+        printf("Average");
+    }
+     if(a=='D')
+    {
+        printf("Below Average");
+    }
+     if(a=='F')
+    {
+        printf("Fail");
+    }
+}
